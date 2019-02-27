5ca07fb1f6a39f0e1848b2a0bede3829a6c6d14b
Rohit Chhetri <rohit.chhetri@gmail.com>
2019-02-26 18:35:44 -0800

    update

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="bottom">
+        Hollywood Actors Click Game <img alt="react" src="assets/images/holly.jpg" />
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/NavMessage/index.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import "./style.css";
+
+// NavMessage renders an li tag containing an message for the user
+
+class NavMessage extends Component {
+  state = {
+    message: "",
+    animating: false
+  };
+
+  componentDidUpdate({ score, topScore }, prevState) {
+    const newState = { animating: true };
+
+    if (score === 0 && topScore === 0) {
+      newState.message = "";
+    } else if (score === 0 && topScore > 0) {
+      newState.message = "incorrect";
+    } else {
+      newState.message = "correct";
+    }
+
+    if (score !== this.props.score || this.state.message !== newState.message) {
+      this.setState(newState);
+    }
+  }
+
+  renderMessage = () => {
+    switch (this.state.message) {
+    case "correct":
+      return "Right Guess!";
+    case "incorrect":
+      return "Wrong Guess!";
+    default:
+      return "Click an image to begin!";
+    }
+  };
+
+  render() {
+    return (
+      <li
+        className={this.state.animating ? this.state.message : ""}
+        onAnimationEnd={() => this.setState({ animating: false })}
+      >
+        {this.renderMessage()}
+      </li>
+    );
+  }
+}
+
+export default NavMessage;
